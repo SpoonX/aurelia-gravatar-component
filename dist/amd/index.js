26587448ff95ae2b7970a40f9d60e4d377eb45cb
bas080 <bas080@hotmail.com>
2016-06-20 16:18:03 +0200

    chore(build): version: 0.0.2

--- dist/amd/index.js
+++ dist/amd/index.js
@@ -5,7 +5,13 @@ define(['exports'], function (exports) {
     value: true
   });
   exports.configure = configure;
-  function configure(config) {
-    config.globalResources('./gravatar');
+  function configure(app) {
+    var configCallback = arguments.length <= 1 || arguments[1] === undefined ? function () {} : arguments[1];
+
+    var config = app.container.get(Config);
+
+    typeof configCallback === 'function' && configCallback(config);
+
+    app.globalResources('./gravatar');
   }
 });
--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -4,6 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.configure = configure;
-function configure(config) {
-  config.globalResources('./gravatar');
+function configure(app) {
+  var configCallback = arguments.length <= 1 || arguments[1] === undefined ? function () {} : arguments[1];
+
+  var config = app.container.get(Config);
+
+  typeof configCallback === 'function' && configCallback(config);
+
+  app.globalResources('./gravatar');
 }
